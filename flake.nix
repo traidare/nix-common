a5228d1b1836a3b3c030e4a1b172a131176c7684
traidare <traidare@proton.me>
2025-07-02 15:44:23 +0200

    Add `nixos-deploy` package

--- flake.nix
+++ flake.nix
@@ -1,18 +1,13 @@
 {
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} (
-      {
-        config,
-        lib,
-        ...
-      }: {
+      {config, ...}: {
         imports = [
           ./lib
           ./packages
         ];
 
         systems = ["x86_64-linux"];
-
         flake.nixosModules = config.flake.lib.dirToAttrs ./config;
       }
     );
@@ -20,5 +15,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    nixos-anywhere = {
+      url = "github:nix-community/nixos-anywhere";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+      };
+    };
+    nix-config-helper = {
+      url = "git+https://codeberg.org/traidare/nix-config-helper.git";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+      };
+    };
   };
 }
