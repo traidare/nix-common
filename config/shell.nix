b192adb5e7ad1cdffc57511b22280577dba9e624
traidare <traidare@proton.me>
2025-07-24 11:54:57 +0200

    Add fish shell abbreviations and enable zoxide

--- config/shell.nix
+++ config/shell.nix
@@ -6,4 +6,15 @@
   };
 
   programs.command-not-found.enable = false;
+
+  programs.fish = {
+    shellAbbrs = {
+      j = lib.mkDefault "journalctl";
+      s = lib.mkDefault "systemctl";
+      ss = lib.mkDefault "sudo systemctl";
+      us = lib.mkDefault "systemctl --user";
+      v = lib.mkDefault "nvim";
+    };
+  };
+  programs.zoxide.enable = lib.mkDefault true;
 }
