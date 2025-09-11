235aa5fca68ae70ce915cb64e11c01434cbf844b
traidare <traidare@proton.me>
2025-09-11 16:56:09 +0200

    Extract packaging functions to `lib/packaging.nix`

---
+++ lib/packaging.nix
@@ -0,0 +1,19 @@
+{lib}: rec {
+  discover = path: transform:
+    lib.pipe (builtins.readDir path) [
+      (lib.filterAttrs (name: value: value == "directory"))
+      transform
+    ];
+
+  discoverWrapperModules = path:
+    if path == null
+    then []
+    else discover path (attrs: map (name: path + "/${name}") (builtins.attrNames attrs));
+
+  discoverPackages = {
+    path,
+    callPackage,
+    extraArgs ? {},
+  }:
+    discover path (builtins.mapAttrs (name: _: callPackage (path + "/${name}") extraArgs));
+}
