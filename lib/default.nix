439a0519842997516ff2eb154203e66529781b5d
traidare <traidare@proton.me>
2025-11-03 18:20:27 +0100

    2025-11-03

--- lib/default.nix
+++ lib/default.nix
@@ -1,36 +1,32 @@
-{inputs, ...}: let
-  lib = inputs.nixpkgs.lib;
-in {
-  flake.lib = rec {
-    dirToAttrs = dir:
-      lib.mapAttrs' (name: value: lib.nameValuePair (lib.removeSuffix ".nix" name) (dir + "/${name}")) (
-        builtins.readDir dir
-      );
+{lib, ...}: rec {
+  dirToAttrs = dir:
+    lib.mapAttrs' (name: value: lib.nameValuePair (lib.removeSuffix ".nix" name) (dir + "/${name}")) (
+      builtins.readDir dir
+    );
 
-    dirToAttrsWithDefault = dir: let
-      modules = dirToAttrs dir;
-      default = {imports = builtins.attrValues (builtins.removeAttrs modules ["default"]);};
-    in
-      modules // {inherit default;};
+  dirToAttrsWithDefault = dir: let
+    modules = dirToAttrs dir;
+    default = {imports = builtins.attrValues (builtins.removeAttrs modules ["default"]);};
+  in
+    modules // {inherit default;};
 
-    versionGate = newPkg: stablePkg: let
-      newVersion = lib.getVersion newPkg;
-      stableVersion = lib.getVersion stablePkg;
-    in
-      if builtins.compareVersions newVersion stableVersion > 0
-      then newPkg
-      else lib.warn "Package ${lib.getName newPkg} reached version >=${newVersion} on stable - stable is now used" stablePkg;
+  versionGate = newPkg: stablePkg: let
+    newVersion = lib.getVersion newPkg;
+    stableVersion = lib.getVersion stablePkg;
+  in
+    if builtins.compareVersions newVersion stableVersion > 0
+    then newPkg
+    else lib.warn "Package ${lib.getName newPkg} reached version >=${newVersion} on stable - stable is now used" stablePkg;
 
-    mkNullOption = type: description:
-      lib.mkOption {
-        type = lib.types.nullOr type;
-        default = null;
-        inherit description;
-      };
+  mkNullOption = type: description:
+    lib.mkOption {
+      type = lib.types.nullOr type;
+      default = null;
+      inherit description;
+    };
 
-    mkNameFromPath = path:
-      builtins.replaceStrings ["/"] ["-"] (lib.removePrefix "/" path);
+  mkNameFromPath = path:
+    builtins.replaceStrings ["/"] ["-"] (lib.removePrefix "/" path);
 
-    packaging = import ./packaging.nix {inherit lib;};
-  };
+  packaging = import ./packaging.nix {inherit lib;};
 }
