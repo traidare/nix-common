192278901233ae0bb0a211966c0795ad2be8ab8c
traidare <traidare@proton.me>
2025-11-22 17:35:27 +0100

    Add documentation comments to library functions

--- lib/default.nix
+++ lib/default.nix
@@ -1,15 +1,23 @@
 {lib, ...}: rec {
+  # Converts a directory to an attrset mapping filenames (without .nix suffix) to their paths
   dirToAttrs = dir:
     lib.mapAttrs' (name: value: lib.nameValuePair (lib.removeSuffix ".nix" name) (dir + "/${name}")) (
       builtins.readDir dir
     );
 
+  # Like dirToAttrs, but adds a 'default' entry that imports all other modules
   dirToAttrsWithDefault = dir: let
     modules = dirToAttrs dir;
-    default = {imports = builtins.attrValues (builtins.removeAttrs modules ["default"]);};
+    default = {
+      imports = builtins.attrValues (
+        # Exclude any existing 'default' to avoid circular imports
+        builtins.removeAttrs modules ["default"]
+      );
+    };
   in
     modules // {inherit default;};
 
+  # Returns `newPkg` if it's newer than `stablePkg`, otherwise returns `stablePkg`
   versionGate = newPkg: stablePkg: let
     newVersion = lib.getVersion newPkg;
     stableVersion = lib.getVersion stablePkg;
@@ -18,6 +26,7 @@
     then newPkg
     else lib.warn "Package ${lib.getName newPkg} reached version >=${newVersion} on stable - stable is now used" stablePkg;
 
+  # Creates a nullable option with a default of null
   mkNullOption = type: description:
     lib.mkOption {
       type = lib.types.nullOr type;
@@ -25,6 +34,7 @@
       inherit description;
     };
 
+  # Converts a path to a name by stripping the leading '/' and replacing '/' with '-'
   mkNameFromPath = path:
     builtins.replaceStrings ["/"] ["-"] (lib.removePrefix "/" path);
 
